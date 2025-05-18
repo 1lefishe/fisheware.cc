672f70ef2547d89f42500294daa0fad05042e798
1lefishe <63523609+1lefishe@users.noreply.github.com>
2025-05-18 18:48:23 -0300

    Update Utilities.lua

--- Utilities.lua
+++ Utilities.lua
@@ -1,7 +1,7 @@
 getgenv().getservices = function(...)
   local new = {};
   for _, service in next, {...} do
-    table.insert(new, game:GetService(service))
+    table.insert(new, cloneref(game:GetService(service)))
   end
   return unpack(new)
 end
